796858932c7e5fbff04b1bcc8f64bef83b91bbc7
pencil78 <sjt777sjt@gmail.com>
2024-10-17 19:02:18 +0000

    commiting task 2

---
+++ assignment_2/task_2/simple_encryption_function.m
@@ -0,0 +1,9 @@
+% Author Name: Sarah Truitt
+% Email: Truitt46@rowan.edu
+% Course: MATLAB Programming - Fall 2024
+% Assignment: Assignment 2
+% Task: Simple Encryption Function
+% Date: 10/11/24
+
+
+encrypt = caesar_cipher('hello',5);
